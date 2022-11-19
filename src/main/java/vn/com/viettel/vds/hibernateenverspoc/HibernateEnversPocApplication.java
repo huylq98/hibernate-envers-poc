f084f284a96f841ea17c2ecb86ec62ed647073c6
huylq <quochuy.2.10.98@gmail.com>
2022-11-19 12:28:13 +0700

    Initial commit

---
+++ src/main/java/vn/com/viettel/vds/hibernateenverspoc/HibernateEnversPocApplication.java
@@ -0,0 +1,13 @@
+package vn.com.viettel.vds.hibernateenverspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateEnversPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateEnversPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/com/viettel/vds/hibernateenverspoc/HibernateEnversPocApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.viettel.vds.hibernateenverspoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateEnversPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
